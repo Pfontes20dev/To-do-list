9b7f819e46cb00544886d6bd9824b03bf4509bdd
Pfontes20dev <pedro.fontes18@yahoo.com>
2026-02-13 15:57:50 -0300

    Usando o .trim()

--- scripts.js
+++ scripts.js
@@ -3,20 +3,20 @@ const hearInput = document.querySelector('.input')
 const eventList = document.querySelector('.event-list')
 
 
-function CallButtonInput(){
-  if (hearInput.value === '') {
-    alert('É necessário que você digite uma tarefa')
+function CallButtonInput() {
+  if (hearInput.value.trim() === '') {
+    alert('⚠️ É necessário que você digite uma tarefa ⚠️')
   } else {
-  const newTask = document.createElement('li')
+    const newTask = document.createElement('li')
 
-  newTask.innerHTML = hearInput.value
+    newTask.innerHTML = hearInput.value
 
-  eventList.appendChild(newTask)
+    eventList.appendChild(newTask)
 
-  const message = 'Tarefa adicionada com sucesso!'
-  document.querySelector('.confimation').innerHTML = message
+    const message = 'Tarefa adicionada com sucesso!'
+    document.querySelector('.confimation').innerHTML = message
 
-  hearInput.value = ''
+    hearInput.value = ''
   }
 }
 
